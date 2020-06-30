8b0cd6dd1da7a259347105d8e2f1bddda9cdd813
ismaeelakram <mathcenter562@gmail.com>
2020-06-30 15:20:29 -0400

    Big update! 2.3
    Now, there is a client and server so you just need a single command. the other way was unreliable because of GitHub API rate-limiting.

--- bin/launchnow.js
+++ bin/launchnow.js
@@ -1,12 +1,23 @@
 #!/usr/bin/env node
 
 const chalk = require("chalk");
+const express = require("express");
 const path = require("path");
 const package = require(path.join(process.cwd(), "package.json"));
 const { default: Axios } = require("axios");
 const { exec } = require("child_process");
 const fs = require("fs");
 const respawn = require("respawn");
+
+const args = process.argv.splice(2);
+
+let type = "none";
+if (args[0].includes("server")) {
+  type = "server";
+}
+if (args[0].includes("client")) {
+  type = "client";
+}
 
 // LOGGING
 function info(x) {
@@ -25,10 +36,6 @@ function good(x) {
   console.log(chalk.green("[👍]") + " " + chalk.green(x));
 }
 
-let user;
-let repo;
-let check_interval;
-
 function sleep(ms) {
   return new Promise((resolve) => {
     setTimeout(resolve, ms);
@@ -43,119 +50,91 @@ function get(dictionary, key) {
     return null;
   }
 }
-// Check if .git exists
-if (!fs.existsSync(path.relative(process.cwd(), ".git"))) {
-  danger("No .git folder found!");
-  danger(
-    "You need to add a remote to the repository. Learn how to add a remote here: https://help.github.com/en/github/using-git/adding-a-remote"
-  );
-  danger(
-    "If you already have a remote, make sure you are running launchnow in the root directory."
-  );
-  process.exit();
-}
 
-if (get(package.launchnow, "check_interval") == null) {
-  warning("No check interval specified. Defaulting to 1 minute.");
-  check_interval = 1;
-} else {
-  check_interval = package.launchnow.check_interval;
-}
-
-if (get(package.launchnow, "check_interval") < 1) {
-  warning("Check interval has to be a minimum of 1. Setting to default of 1.");
-  check_interval = 60000;
-}
-
-// Check if github_repo exists
-if (get(package.launchnow, "github_repo") == null) {
-  danger("No github repository set in package.json! ");
-  danger(
-    "Please read our github README (https://github.com/novmbr/launchnow/README.md) on the proper way of setting a script and repository."
-  );
-  process.exit();
-}
-// Check if script exists
-if (get(package.launchnow, "script") == null) {
-  danger("No script set in package.json! ");
-  danger(
-    "Please read our github README (https://github.com/novmbr/launchnow/README.md) on the proper way of setting a script and repository."
-  );
-  process.exit();
-}
-
-good(
-  "GitHub repository recognized as: " +
-    chalk.reset(chalk.bold(package.launchnow.github_repo))
-);
-good(
-  `Script recognized as: ` + chalk.reset(chalk.bold(package.launchnow.script))
-);
-good(
-  "Check interval recognized as: " + chalk.reset(chalk.bold(check_interval))
-);
-user = package.launchnow.github_repo.split(":")[0];
-repo = package.launchnow.github_repo.split(":")[1];
-
-info("Checking if github repository is accessible...");
-Axios.get(`https://api.github.com/repos/${user}/${repo}`)
-  .then((res) => {
-    good("Repository is accessible.");
-  })
-  .catch((err) => {
-    danger("Repository is either private or does not exist.");
-    proc.stop();
+if (type == "server") {
+  // Check if .git exists
+  if (!fs.existsSync(path.relative(process.cwd(), ".git"))) {
+    danger("No .git folder found!");
+    danger(
+      "You need to add a remote to the repository. Learn how to add a remote here: https://help.github.com/en/github/using-git/adding-a-remote"
+    );
+    danger(
+      "If you already have a remote, make sure you are running launchnow in the root directory."
+    );
     process.exit();
+  }
+  // Check if script exists
+  if (get(package.launchnow, "script") == null) {
+    danger("No script set in package.json! ");
+    danger(
+      "Please read our github README (https://github.com/novmbr/launchnow/README.md) on the proper way of setting a script and repository."
+    );
+    process.exit();
+  }
+
+  good(
+    "GitHub repository recognized as: " +
+      chalk.reset(chalk.bold(package.launchnow.github_repo))
+  );
+  good(
+    `Script recognized as: ` + chalk.reset(chalk.bold(package.launchnow.script))
+  );
+
+  let latestCommit = "";
+
+  var proc = respawn(package.launchnow.script.split(" "), {
+    name: "script", // set monitor name
+    cwd: ".", // set cwd
+    maxRestarts: -1, // how many restarts are allowed within 60s
+    // or -1 for infinite restarts
+    sleep: 1000, // time to sleep between restarts,
+    fork: false, // fork instead of spawn
   });
 
-let latestCommit = "";
+  proc.start();
 
-var proc = respawn(package.launchnow.script.split(" "), {
-  name: "script", // set monitor name
-  cwd: ".", // set cwd
-  maxRestarts: -1, // how many restarts are allowed within 60s
-  // or -1 for infinite restarts
-  sleep: 1000, // time to sleep between restarts,
-  fork: false, // fork instead of spawn
-});
+  proc.on("stdout", (data) => {
+    console.log(data.toString());
+  });
 
-proc.start();
+  info("Process started.");
 
-proc.on("stdout", (data) => {
-  console.log(data.toString());
-});
-
-info("Process started.");
-
-function checkPush() {
-  info("Checking for new push...");
-  Axios.get(`https://api.github.com/repos/${user}/${repo}/commits/master`)
-    .then((res) => {
-      if (latestCommit == "") {
-        latestCommit = res.data.sha;
-        return;
+  function fetchAndRestart() {
+    info("Fetching...");
+    exec("git fetch", (err, stdout, stderr) => {
+      if (err) {
+        danger("Error: " + err);
+        process.exit();
       }
-      if (res.data.sha != latestCommit) {
-        proc.stop(function () {
-          info("New push! Fetching....");
-          exec("git fetch", (err, stdout, stderr) => {
-            if (err) {
-              danger("Error: " + err);
-              process.exit();
-            }
-          }).on("close", (code, signal) => {
-            proc.start();
-            good("Restarted.");
-            latestCommit = res.data.sha;
-          });
-        });
-      }
+    }).on("close", (code, signal) => {
+      proc.start();
+      good("Restarted.");
+    });
+  }
+
+  const app = express();
+
+  app.get("/deploy", (req, res) => {
+    fetchAndRestart();
+    return res.status(200);
+  });
+
+  app.listen(2424, () => good("Launchnow server running on port 2424."));
+} else if (type == "client") {
+  let serverIP = args[1];
+  Axios.get(`http://${serverIP}:2424/deploy`)
+    .then(() => {
+      good("Sent deploy request.");
     })
     .catch((err) => {
-      danger("Repository is either private or does not exist.");
-      process.exit();
+      danger("Server not reachable.");
     });
+} else if (type == "none") {
+  danger(
+    "Are you running a launchnow server or are you trying to deploy? Use" +
+      chalk.white("launchnow server") +
+      chalk.red(" to run a server and ") +
+      chalk.white("launchnow client <IP>") +
+      chalk.red(" to re-deploy a server.")
+  );
 }
-
-checkPush();
-setInterval(checkPush, check_interval * 60000);
